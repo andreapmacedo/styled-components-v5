1d7abed471820f78443e5e4a747131d9d75a5c02
andre macedo <aa.andremacedo@gmail.com>
2022-07-20 00:15:00 -0300

    feito: adiciona aula 11 - first styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import styled from "styled-components"
+
+const BasicTitle = styled.h1`
+  text-align: center;
+  text-transform: capitalize;
+`
+
+
 function App() {
   return (
     <div style={{ padding: '2rem', backgroundColor: "white"}}>
       <h1 className="title">Styled Components </h1>
+      <BasicTitle>styled component</BasicTitle>
+      <BasicTitle>styled component</BasicTitle>
+      <BasicTitle>styled component</BasicTitle>
+      <BasicTitle>styled component</BasicTitle>
+      <BasicTitle>styled component</BasicTitle>
       <button className="btn">click me</button>
-      <div style={{ padding: '2rem'}}>
-        <h2 className="title">hello world</h2>
-      </div>
     </div>  
   )
 }
